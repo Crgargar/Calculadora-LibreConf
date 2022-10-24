67725633336dff1be8f74c918eecbfbb01b80188
García Garrido, Cristian <94846219+Crgargar@users.noreply.github.com>
2022-10-24 10:03:27 +0200

    calculadora-LibreConf

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  num1: number;
+  num2: number;
+  numS: number;
+  numR: number;
+  numM: number;
+  numD: number;
+
+
+  suma() {
+    this.numS= this.num1 + this.num2;
+    console.log(this.numS);
+  }
+
+  resta() {
+    this.numR= this.num1 - this.num2;
+    console.log(this.numR);
+  }
+
+  multiplicacion() {
+    this.numM= this.num1 * this.num2;
+    console.log(this.numM);
+  }
+
+  division() {
+    this.numD= this.num1 / this.num2;
+    console.log(this.numD);
+  }
+
+  reinicia() {
+    this.num1=null;
+    this.num2=null;
+    this.numS=null;
+    this.numR=null;
+    this.numM=null;
+    this.numD=null;
+
+  }
 
 }
